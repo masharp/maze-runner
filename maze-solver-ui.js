3ddd6b5b86e558b4e3514edc9b966006d5d7a4bd
Michael Sharp <msharp.oh@gmail.com>
2017-06-14 10:14:04 -0700

    added some more structure

--- maze-solver-ui.js
+++ maze-solver-ui.js
@@ -4,3 +4,40 @@
     www.softwareontheshore.com
 */
 'use strict';
+const imageGenerationAPI = 'http://localhost:3000/api/maze';
+const analysis = {
+    totalNodes: 0,
+    graphNodes: 0,
+    completionTime: 0,
+};
+
+/* function that processes the generated maze image into a multi dimensional array */
+function processMaze(maze) {
+    const arr = [];
+};
+
+/* function that draws the solution onto the map */
+function drawSolution(graph, maze) {
+    const newImage = null;
+};
+
+/* function that takes the paths of the maze and graphs the important decisions */
+function graphMaze(arr) {
+    const graph = {};
+};
+
+/* function that solves the maze using BFS */
+function solveBFS(graph) {
+    const solution = {};
+};
+
+/* function that solves the maze using DFS */
+funtion solveDFS(graph) {
+    const solution = {};
+};
+
+/* function that solves the maze using disjktras */
+function solveDikjstras(graph) {
+    const solution = {};
+
+};
